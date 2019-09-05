aa451f8ee716e0db06e72e72e31290ed5399cd7a
Adriano Chambel <acmlima.softweb@gmail.com>
2019-09-05 23:47:52 +0100

    Add styles to Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,101 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://resources.mandmdirect.com/Images/_default/n/b/1/nb1560_1_cloudzoom.jpg"
+          alt="trainer"
+        />
+        <strong>Cool trainer</strong>
+        <span>£29.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to Cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://resources.mandmdirect.com/Images/_default/n/b/1/nb1560_1_cloudzoom.jpg"
+          alt="trainer"
+        />
+        <strong>Cool trainer</strong>
+        <span>£29.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to Cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://resources.mandmdirect.com/Images/_default/n/b/1/nb1560_1_cloudzoom.jpg"
+          alt="trainer"
+        />
+        <strong>Cool trainer</strong>
+        <span>£29.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to Cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://resources.mandmdirect.com/Images/_default/n/b/1/nb1560_1_cloudzoom.jpg"
+          alt="trainer"
+        />
+        <strong>Cool trainer</strong>
+        <span>£29.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to Cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://resources.mandmdirect.com/Images/_default/n/b/1/nb1560_1_cloudzoom.jpg"
+          alt="trainer"
+        />
+        <strong>Cool trainer</strong>
+        <span>£29.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to Cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://resources.mandmdirect.com/Images/_default/n/b/1/nb1560_1_cloudzoom.jpg"
+          alt="trainer"
+        />
+        <strong>Cool trainer</strong>
+        <span>£29.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to Cart</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
