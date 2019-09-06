8ff5549dc86dd979308da366e5b31af9341e390b
Adriano Chambel <acmlima.softweb@gmail.com>
2019-09-06 21:50:56 +0100

    Add styles to Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Product</th>
+            <th>Qty</th>
+            <th>Subtotal</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://resources.mandmdirect.com/Images/_default/n/b/1/nb1560_1_cloudzoom.jpg"
+                alt="trainer"
+              />
+            </td>
+            <td>
+              <strong>Cool Trainer</strong>
+              <span>£29.90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>£59.80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Processed to Checkout</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>$1920.55</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
